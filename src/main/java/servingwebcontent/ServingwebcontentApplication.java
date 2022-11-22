d0f0d20ae6bd53f730416b0003cdb61b2b835433
gentlable <shingo.gt.1115@gmail.com>
2022-11-22 23:20:50 +0900

    first commit

---
+++ src/main/java/servingwebcontent/ServingwebcontentApplication.java
@@ -0,0 +1,13 @@
+package servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingwebcontentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingwebcontentApplication.class, args);
+	}
+
+}
